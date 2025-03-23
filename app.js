0a08a0112d427ea4919362ae784544311181bd59
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-03-23 23:39:59 +0530

    Learnt about HTTP methods and routing

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { searchController, usernameController } from "./controller.js";
+
+// import { searchController, usernameController } from "./controller.js";
+import { userLogin, userSignup } from "./controller.js";
+import router from "./route.js";
 
 const app = express();
 
@@ -10,10 +13,53 @@ app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
-// ---------Dynamic routing-----------
-app.get('/user/:username',usernameController)
+// Handle multiple route parameters & also using regex
+app.get("/things/:name/:id([0-9]{5})", (req, res)=> {
+  const {name,id} = req.params;
+  res.json({
+    id,
+    name
+  })
+})
 
-app.get("/search",searchController)
+// ---------Dynamic routing-----------
+// app.get('/user/:username',usernameController)
+// app.get("/search",searchController)
+// app.get("/user/login", userLogin);
+// app.get("/user/signup", userSignup);
+app.use('/user', router);
+
+// POSt Methods in HTTP Methods
+app.use(express.json());
+
+app.post("/users", (req, res)=> {
+  const {name, email} = req.body;
+  res.json({
+    message: `User ${name} with email ${email} created successfully!`
+  })
+})
+
+// PUT Request in HTTP Methods
+app.put("/users/:id", (req, res)=> {
+  const userId = req.params.id;
+  const {name, email} = req.body;
+  res.json({
+    message: `User ${userId} updated to ${name}, ${email}`
+  })
+})
+
+// Delete Request in HTTP Methods
+app.delete("/users/:id", (req, res)=> {
+  const userId = req.params.id;
+  res.json({
+    message: `User with id ${userId} deleted successfully!`
+  })
+})
+
+// Catch all invalid routes
+app.get("*", (req, res)=> {
+  res.send("Sorry, this is an invalid URL.")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- controller.js
+++ controller.js
@@ -1,10 +1,18 @@
-export const usernameController = (req, res) => {
-    const username = req.params.username;
-    res.send(`Welcome ${username}`);
-}
+// export const usernameController = (req, res) => {
+//     const username = req.params.username;
+//     res.send(`Welcome ${username}`);
+// }
 
 // /search?keyword=express
-export const searchController = (req, res) => {
-    const keyword = req.query.keyword;
-    res.send(`Searching for ${keyword}`);
+// export const searchController = (req, res) => {
+//     const keyword = req.query.keyword;
+//     res.send(`Searching for ${keyword}`);
+// }
+
+//---------userLogin-----------
+export const userLogin = (req, res) => {
+    res.send("This is user Login route!")
+}
+export const userSignup = (req, res) => {
+    res.send("This is user Signup route!")
 }
